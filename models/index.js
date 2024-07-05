17fc3673a715d25c227bb5ec4c23d5598aededef
chansik0504 <chansik0504@gmail.com>
2024-07-05 18:29:24 +0900

    배포 test

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectMongoDB = async () => {
     try {
         // await mongoose.connect('mongodb://mongodb1:27017/DokTest'); // 도커 서버 실행 시
-        await mongoose.connect('mongodb://localhost:27016/DokTest'); // 로컬 서버 실행 시
+        // await mongoose.connect('mongodb://localhost:27016/DokTest'); // 로컬 서버 실행 시
+        await mongoose.connect(process.env.MONGO_URI); // 배포 서버
         console.log('MongoDB connected');
     } catch(err) {
         console.error(err.message);
