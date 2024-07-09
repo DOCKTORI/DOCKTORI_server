7bdce7e74858cf2b36544a99c9effdfe36e1819b
chansik0504 <chansik0504@gmail.com>
2024-07-10 02:03:50 +0900

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -90,7 +90,7 @@ const login = async (req, res) => {
     user.refreshToken = refreshToken;
     await user.save();
 
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false });
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none', path: '/', });
 
     return res.status(StatusCodes.OK).json({
       nickname: user.nickname,
