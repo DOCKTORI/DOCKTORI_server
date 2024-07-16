2f099fec26e13c9cdb83679d1aeb710562da0a6b
chansik0504 <chansik0504@gmail.com>
2024-07-16 16:32:09 +0900

    cors origin fix

--- controllers/book.js
+++ controllers/book.js
@@ -26,7 +26,6 @@ const addDeletelike = async (req, res) => {
 
 const searchBooks = async (req, res) => {
     const title = decodeURIComponent(req.query.title);
-    console.log(title);
     const url = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${process.env.ALADIN_TTBKEY}&Query=${title}&QueryType=Title&MaxResults=20&Sort=Accuracy&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
     try {
